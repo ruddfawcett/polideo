e7505177be875f415014ef7b64650fcb1d28f62c
Rudd Fawcett <rudd.fawcett@gmail.com>
2018-04-15 14:07:39 -0400

    Completely revamped algorithm

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,7 @@
-const APP_ID = '503600090034639'
-const ACCESS_TOKEN = 'EAACEdEose0cBAJNGPFdN2GYmX10gZChzT3tI7kKbU08UZCWbL7KJ8pyIvdUwkdRtZB0MY3xbjZBHnSVrfZA645rat0ZBLgqUj9HXlkQZAbqPQd7ZAZCzd7XxF43U6GPce1CNZBkBfKQD3iUtDteZBFnwM3dl5focso36SgdMzLFTjD9gFDkO8PmW8dnZAWeTVHnnYeI3J9z3xIMK7CqJu7M9ZBZC4G'
+const EVP_ID = '503600090034639'
+const ACCESS_TOKEN = ''
+
+const TESTING_MATH = true;
 
 const POST_TOPICS = ['president-trump', 'health-care', 'guns', 'abortion', 'isis', 'budget', 'executive-order', 'immigration'];
 const POINT_VALUES = {
@@ -10,70 +12,55 @@ const POINT_VALUES = {
 }
 
 var PMath = {
+  VOLATILITY: -1,
+  PPV: 0,
+  EVMAX: 5,
   AR: function(sumA, totN) {
     return sumA / totN;
   },
-  AE: function(AR, sumAPV) {
-    return AR * sumAPV;
+  AE: function(AR, sumEVV) {
+    return AR * sumEVV;
   },
   IA: function(AEm, AEM) {
-    return AEm / AEM;
+    return Math.abs(AEm) / Math.abs(AEM);
   },
-  maxAP: 5,
-  fPV: function(IA, AP, PV) {
-    return PV + this.fDPV(IA, AP, PV);
+  fPV: function(IA, EV, PV) {
+    return PV + this.fDPV(IA, EV, PV);
   },
-  fDPV: function(IA, AP, PV) {
-    if (Math.abs(AP + PV) >= Math.abs(PV)) {
-      return (AP / this.maxAP) * Math.pow((1 - Math.abs(PV)), 2);
+  fDPV: function(IA, EV, PV) {
+    if (Math.abs(EV + PV) >= Math.abs(PV)) {
+      return (EV / this.EVMAX) * Math.pow((1 - Math.abs(PV)), 2);
     }
     else {
-      return -1 * (AP / this.maxAP) * Math.pow((1 - Math.abs(PV)), 2) * IA;
+      return this.VOLATILITY * (EV / this.EVMAX) * Math.pow((1 - Math.abs(PV)), 2) * IA;
     }
   }
 }
 
 var App = {
-  calculate: function(AP) {
+  // Assumes that there is already a post with a PV.
+  calculate: function(EV, PV) {
     let totN = this.db().count();
     let lN = this.db({alignment: 'left'}).count();
     let rN = this.db({alignment: 'right'}).count();
 
-    if (lN == 0 || rN == 0) {
-      return;
-    }
+    let lsumEV = this.db({alignment: 'left'}).sum('ev');
+    let rsumEV = this.db({alignment: 'right'}).sum('ev');
 
-    let lsumPV = this.db({alignment: 'left'}).sum('dp');
-    let rsumPV = this.db({alignment: 'right'}).sum('dp');
+    var lAR = PMath.AR(lN, totN);
+    var rAR = PMath.AR(rN, totN);
 
-    var AEm = 0;
-    var AEM = 0;
+    var lAE = PMath.AE(lAR, lsumEV);
+    var rAE = PMath.AE(lAE, rsumEV);
 
-    // Left is the alignment minority or it's arbitrary (they are equal).
-    if (lN <= rN) {
-      var ARm = PMath.AR(lN, totN);
-      var ARM = PMath.AR(rN, totN);
+    let IA = (Math.abs(lAE) <= Math.abs(rAE)) ? PMath.IA(lAE, rAE) : PMath.IA(rAE, lAE);
+    let nPV = PMath.fPV(IA, EV, PV);
 
-      AEm = PMath.AE(ARm, lsumPV);
-      AEM = PMath.AE(ARM, rsumPV);
-    }
-    // Right is the alignment minority.
-    else {
-      var ARm = PMath.AR(rN, totN);
-      var ARM = PMath.AR(lN, totN);
-
-      AEm = PMath.AE(ARm, rsumPV);
-      AEM = PMath.AE(ARM, lsumPV);
-    }
-
-    let IA = PMath.IA(AEm, AEM);
-    let PV = this.db().last().point_value;
-
-    let calculatedValue = PMath.fPV(IA, AP, PV);
-
-    console.log(`fPV(IA, AP, PV) = ${calculatedValue}`);
-    $('#fPV-value').html(`\\[f_{PV}(IA, \ AP,\ PV) = ${calculatedValue}\\]`);
+    console.log(`fPV(IA, EV, PV) = ${nPV}`);
+    $('#fPV-value').html(`\\[f_{PV}(IA, \ EV,\ PV) = ${nPV}\\]`);
     MathJax.Hub.Queue(['Typeset', MathJax.Hub, 'fPV-value']);
+
+    return nPV;
   },
   start: function() {
     var _this = this;
@@ -96,7 +83,7 @@ var App = {
     $.ajaxSetup({ cache: true });
     $.getScript('https://connect.facebook.net/en_US/sdk.js', function(){
       FB.init({
-        appId: APP_ID,
+        EVpId: EVP_ID,
         version: 'v2.12'
       });
 
@@ -104,8 +91,10 @@ var App = {
     });
   },
   handle_action: function(node) {
-    if (!this.should_insert) return;
-    this.should_insert = false;
+    if (!TESTING_MATH) {
+      if (!this.should_insert) return;
+      this.should_insert = false;
+    }
 
     var _this = this;
 
@@ -114,30 +103,34 @@ var App = {
     let alignment = post.attr('data-alignment');
 
     let sign = alignment == 'left' ? -1 : 1;
-    let dp = sign * POINT_VALUES[action_tag];
+    let EV = sign * POINT_VALUES[action_tag];
 
     let index = 1;
-    let point_value = dp;
+    let engagement_value = EV;
 
     let last_entry = this.db().last();
 
+    // Initial P value as a raio of your engagement value * a constant.
+    var PV = EV * 0.05;
+
     if (last_entry) {
       index = last_entry.index + 1;
-      point_value = last_entry.point_value + dp;
+      engagement_value = last_entry.ev_sum + EV;
+      PV = this.calculate(EV, last_entry.pv);
     }
 
     var row = {
       'index': index,
       'alignment': alignment,
-      'dp': dp,
-      'point_value': point_value,
+      'ev': EV,
+      'ev_sum': engagement_value,
+      'pv': PV,
       'post': post.attr('data-permalink_url'),
       'post_source': post.attr('data-from')
     }
 
     if (this.db.insert(row)) {
       this.insert_row(row);
-      this.calculate(Math.abs(dp));
       this.fetch_post();
     }
     else {
@@ -146,7 +139,7 @@ var App = {
   },
   lookup_page: function(pagename) {
     var P = $.Deferred();
-    FB.api(`/${pagename}`, {
+    FB.EVi(`/${pagename}`, {
       access_token: ACCESS_TOKEN
     }, function(response) {
       if (!response) {
@@ -162,7 +155,7 @@ var App = {
   },
   lookup_post: function(page_id, post_id) {
     var _this = this;
-    FB.api(`/${page_id}_${post_id}`, {
+    FB.EVi(`/${page_id}_${post_id}`, {
       access_token: ACCESS_TOKEN,
       fields: 'attachments{media,description,title, url},message,full_picture,created_time,status_type'
     }, function(response) {
@@ -190,7 +183,7 @@ var App = {
             $(`<div class='media' id='post-attachment'>
                   <div class='image' id='post-full_picture'></div>
                   <div class='deck'>
-                    <h1 id='post-attachment-title'>Mei elitr aperiri rationibus id nulla expetenda pro ad</h1>
+                    <h1 id='post-attachment-title'>Mei elitr EVeriri rationibus id nulla expetenda pro ad</h1>
                     <p id='post-attachment-description'>Ea his partem erroribus, est ea labore utroque delectus, sea meliore platonem ut. Ex odio diam voluptatibus sea, doming mollis civibus ea nec.</p>
                     <h6 class='reveal-text' id='post-attachment-source' data-default='website.com'>website.com</h6>
                   </div>
@@ -230,18 +223,16 @@ var App = {
       <tr>
         <td>${row.index}</td>
         <td>${row.alignment}</td>
-        <td>${row.dp}</td>
-        <td>${row.point_value}</td>
-        <td><a href='${row.post}'>Link</a></td>
-        <td>${row.post_source}</td>
+        <td>${row.ev}</td>
+        <td>${row.ev_sum}</td>
+        <td>${row.pv}</td>
+        <!-- <td><a href='${row.post}'>Link</a></td>
+        <td>${row.post_source}</td> -->
       </tr>`);
   },
   setup_post: function(post, alignment) {
     var _this = this;
     $('div.post').attr('data-alignment', alignment);
-    $('div.post').removeClass('left');
-    $('div.post').removeClass('right');
-    $('div.post').addClass(alignment);
     $('div.post').attr('data-permalink_url', post.permalink_url);
     $('div.post').attr('data-from', post.from);
 
@@ -256,11 +247,19 @@ var App = {
   },
   fetch_post: function() {
     var _this = this;
+    let alignment = ['left', 'right'][Math.floor(Math.random() * 2)];
+
+    if (TESTING_MATH) {
+      $('div.post').attr('data-alignment', alignment);
+      $('div.post').removeClass('left');
+      $('div.post').removeClass('right');
+      $('div.post').addClass(alignment);
+      return;
+    }
 
     let topic_idx = Math.floor(Math.random() * POST_TOPICS.length);
     let page_num = Math.floor(Math.random() * 10) + 1;
-    let alignment = ['left', 'right'][Math.floor(Math.random() * 2)];
-    let page_url = `http://graphics.wsj.com/blue-feed-red-feed/data.php?page=${page_num}&keyword=${POST_TOPICS[topic_idx]}`;
+    let page_url = `http://grEVhics.wsj.com/blue-feed-red-feed/data.php?page=${page_num}&keyword=${POST_TOPICS[topic_idx]}`;
     let origin_workaround_url = `http://www.whateverorigin.org/get?url=${encodeURIComponent(page_url)}&callback=?`;
 
     $.getJSON(origin_workaround_url, function(data) {
