43b722e2c76d33f0a05b748181b3357a415ba1fa
Rudd Fawcett <rudd.fawcett@gmail.com>
2018-04-22 21:26:22 -0400

    Fleshed out ways of obtaining data from RFBF

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
-var test = require('./test');
+const test = require('./test');
+const rfbf = require('../util/rfbf');
 
 module.exports = function(router) {
   router.use(function(req, res, next) {
@@ -6,6 +7,12 @@ module.exports = function(router) {
   });
 
   router.get('/test', test.get);
+
+  router.get('/posts/:count', (req, res, next) => {
+    var posts = rfbf.get(req.params.count).then(rfbf.find_page).then(rfbf.match_post).then((data) => {
+      res.json(data);
+    });
+  });
 
   router.use('*', function(req, res) {
     res.status(404).render('404');
---
+++ src/util/rfbf.js
@@ -0,0 +1,101 @@
+var Promise = require('bluebird');
+
+var Graph = require('fbgraph');
+Promise.promisifyAll(Graph);
+
+var request = require('request-promise');
+var url = require('url');
+
+Graph.setAccessToken('EAACEdEose0cBACmJ9L57t6ymliGQxzcnysRf0y4ZAMZBhrYxDXDzpLzhRFGeZCBCzYpsXTGk5QP8yEBGltesVD0nZBK4WvYYOaEduyAqe1nzBNtzD2XDHNdplVi7Rng5gAwTZB0JxLRgwJBIxP3ccEXDBDYdYlWPeXpohLjB4OZAoRhhcvLTY7L14GKN1rcen853eUFK3VeH5ZAFeh6l3Ew');
+
+const POST_TOPICS = ['president-trump', 'health-care', 'guns', 'abortion', 'isis', 'budget', 'executive-order', 'immigration'];
+
+module.exports = {
+  get: (count) => {
+    var tURL = () => {
+      let topic_idx = Math.floor(Math.random() * POST_TOPICS.length);
+      let topic = POST_TOPICS[topic_idx];
+      let page_num = Math.floor(Math.random() * 10) + 1;
+
+      return [
+        `http://graphics.wsj.com/blue-feed-red-feed/data.php?page=${page_num}&keyword=${topic}`,
+        topic
+      ];
+    }
+
+    var requests = [];
+    for (var i = 1; i <= count; i++) {
+      let tuple = tURL();
+      requests.push({
+        url: tuple[0],
+        topic: tuple[1]
+      });
+    }
+
+    return Promise.map(requests, (obj) => {
+      return request(obj).then((body) => {
+        let alignment = ['left', 'right'][Math.floor(Math.random() * 2)];
+
+        var response = JSON.parse(body);
+        let posts = response[alignment].filter((post) => {
+          return post.permalink_url.indexOf('videos') == -1;
+        });
+
+        var post = posts[Math.floor(Math.random() * posts.length)];
+
+        let post_url = url.parse(post.permalink_url);
+        let parts = post_url.pathname.split('/');
+        let pagename = parts[1];
+        let post_id = parts[parts.length - 1];
+
+        delete post.picture;
+        delete post.permalink_url;
+
+        post.alignment = alignment;
+        post.topic = obj.topic;
+        post.pagename = pagename;
+        post.post_id = post_id;
+
+        return post;
+      });
+    });
+  },
+  find_page: (posts) => {
+    return Promise.map(posts, (post) => {
+      return Graph.getAsync(post.pagename, { fields: '' }).then((res) => {
+        post.page_id = res.id;
+        return post;
+      });
+    });
+  },
+  match_post: (posts) => {
+    return Promise.map(posts, (post) => {
+      return Graph.getAsync(`${post.page_id}_${post.post_id}`, {
+        fields: 'attachments{media,description,title, url},message,full_picture,created_time,status_type'
+      }).then((response) => {
+        var data = {};
+
+        data.message = response.message ? response.message : '';
+
+        if (response.status_type == 'added_photos') {
+          if (response.full_picture) {
+            data.full_picture = response.full_picture ? response.full_picture : '';
+          }
+        }
+        else {
+          if (response.attachments && response.attachments.data.length > 0) {
+            let attachment = response.attachments.data[0];
+            data.attachment = {};
+
+            data.attachment.title = attachment.title ? attachment.title : '';
+            data.attachment.description = attachment.description ? attachment.description : '';
+          }
+        }
+
+        post.data = data;
+
+        return post;
+      });
+    });
+  }
+}
